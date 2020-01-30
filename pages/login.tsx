abfd82c1323896dc3891c21677a07b0a87708552
Benjamin Tang <benjamin.tang.hz@gmail.com>
2020-01-30 11:04:30 +0800

    use webkit fill available

--- pages/login.tsx
+++ pages/login.tsx
@@ -50,7 +50,7 @@ const Page: React.FC = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {state.isLoading && <LoadingModal />}
-      <div className="h-screen w-full bg-indigo-100 flex flex-col">
+      <div className="h-full w-full bg-indigo-100 flex flex-col">
         <div className="flex-1" />
         <div className="mb-4 mx-10">
           <div className="text-3xl font-semibold text-black leading-tight">
